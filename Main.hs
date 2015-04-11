bad5f429c6d4fdb9d665db91b252ba82d95de971
Joël Quenneville <joelq@thoughtbot.com>
2015-04-11 13:17:29 -0400

    Interact with stdin/stdout
    Bot correctly reads in commands from stdin and outputs responses to
    stdout. It responds to all actions by calling.

--- Main.hs
+++ Main.hs
@@ -1,2 +1,14 @@
 module Main where
-main = putStr "Welcome to Xenophon"
+
+responseFor :: [String] -> String
+responseFor ("Action":_) = "call 0"
+responseFor _ = ""
+
+processCommand :: String -> String
+processCommand x = responseFor $ words x
+
+processInput :: String -> String
+processInput x = unlines $ map processCommand $ lines x
+
+main :: IO ()
+main = interact processInput
